88f6494ff9dcb3fc0bbb4fff5d736e99680e5264
Duy Linh <duylinhit99@gmail.com>
2024-10-28 15:37:15 +0700

    increase and decrease handle

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -3,8 +3,8 @@ import { MdDelete } from "react-icons/md";
 import api from '../../api';
 import { toast } from 'react-toastify';
 const Cart = () => {
-    const [data, setData] = useState({})
-
+    const [data, setData] = useState([])
+    const [total, setTotal] = useState(0)
     useEffect(() => {
         const cart = JSON.parse(localStorage.getItem('cart'))
         if (cart) {
@@ -14,6 +14,52 @@ const Cart = () => {
         }
     }, [])
 
+    useEffect(() => {
+        const totalProduct = data.reduce((total, value) => {
+            const price = Number(value.price) || 0; // Chuyển đổi về số nếu có thể
+            const qty = Number(value.quantity) || 0; // Chuyển đổi về số nếu có thể
+            return total + (price * qty);
+        }, 0);
+        console.log(data);
+
+        setTotal(totalProduct)
+    }, [data])
+
+
+    const handleIncrease = (id) => {
+        const newData = data.map((item) =>
+            item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+
+        setData(newData)
+        const getCart = JSON.parse(localStorage("cart")) || {}
+        if (getCart[id] && getCart[id] > 1) {
+            getCart[id] += 1
+        }
+        localStorage.setItem("cart", JSON.stringify(getCart))
+        let tongQty = Object.values(getCart).reduce((sum, qty) => sum + qty, 0)
+        localStorage.setItem("tongqty", JSON.stringify(tongQty))
+    }
+
+    const hanldeReduce = (id) => {
+        const newData = data.map((item) =>
+            item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+        )
+            .filter(item => item.quantity > 0)
+        setData(newData)
+
+        const getCart = JSON.parse(localStorage.getItem("cart")) || {}
+        if (getCart[id] && getCart[id] > 1) {
+            getCart[id] -= 1
+        } else {
+            delete getCart[id]
+        }
+        localStorage.setItem("cart", JSON.stringify(getCart));
+        console.log(getCart);
+        let tongQty = Object.values(getCart).reduce((sum, qty) => sum + qty, 0);
+        localStorage.setItem("tongQty", JSON.stringify(tongQty));
+    }
+
     const hanldeDelete = (e) => {
         // Lấy ID từ sự kiện
         let id = e.currentTarget.id
@@ -21,15 +67,13 @@ const Cart = () => {
             console.error("ID không được tìm thấy!");
             return;
         }
-        // tạo newData
-        const newData = data.filter(item => item.id.toString() !== id); // Chuyển đổi ID thành chuỗi để so sánh
-        // Nếu không có sản phẩm nào trong newData, log cảnh báo
+
+        const newData = data.filter(item => item.id.toString() !== id);
         if (newData.length === data.length) {
             console.warn(`Sản phẩm với ID ${id} không tồn tại.`);
             return;
         }
-        // lấy cart local ra 
-        // Cập nhật cart trong localStorage
+
         let cart = localStorage.getItem('cart');
         if (cart) {
             cart = JSON.parse(cart);
@@ -39,29 +83,37 @@ const Cart = () => {
         setData(newData)
         toast.success("Delete successfully")
     }
+
+    const handleQuantityChange = (index, event) => {
+        const newQuantity = event.target.value;
+        const newData = [...data];
+        newData[index].quantity = newQuantity; // Cập nhật số lượng
+        setData(newData); // Cập nhật trạng thái
+    };
+
     const renderData = () => {
         if (Object.keys(data).length > 0) {
             return Object.keys(data).map((item, i) => {
                 return (
                     <>
                         {/* <!-- Sản phẩm 1 --> */}
-                        <tr tr class="border-b transition duration-300" >
-                            <td class="py-2 px-6 flex items-center">
-                                <img class="w-20 h-20 rounded-md mr-4 shadow" src={data[item].image} alt="Product Image" />
-                                <span class="text-lg font-semibold text-gray-800 dark:text-white">{data[item].title}</span>
+                        <tr className="border-b transition duration-300" key={item}>
+                            <td className="py-2 px-6 flex items-center">
+                                <img className="w-20 h-20 rounded-md mr-4 shadow" src={data[item].image} alt="Product Image" />
+                                <span className="text-lg font-semibold text-gray-800 dark:text-white">{data[item].title}</span>
                             </td>
-                            <td class="py-2 px-6">
-                                <div class="flex items-center justify-center border-gray-300 rounded">
-                                    <button class="px-2 py-1 bg-gray-200 text-gray-600 hover:bg-gray-300">-</button>
-                                    <input type="text" class="w-12 text-center border-none outline-none" value={data[item].quantity} />
-                                    <button class="px-2 py-1 bg-gray-200 text-gray-600 hover:bg-gray-300">+</button>
+                            <td className="py-2 px-6">
+                                <div className="flex items-center justify-center border-gray-300 rounded">
+                                    <button type='button' className="px-2 py-1 bg-gray-200 text-gray-600 hover:bg-gray-300" onClick={() => hanldeReduce(data[item].id)}>-</button>
+                                    <input type="text" className="w-12 text-center border-none outline-none" value={data[item].quantity} onChange={(event) => handleQuantityChange(index, event)} />
+                                    <button type='button' className="px-2 py-1 bg-gray-200 text-gray-600 hover:bg-gray-300" onClick={() => handleIncrease(data[item].id)}>+</button>
                                 </div>
                             </td>
-                            <td class="py-2 px-6 text-center">
-                                <span class="text-lg font-semibold text-gray-800 dark:text-white" id={data[item].id}>{data[item].quantity * data[item].price}$</span>
+                            <td className="py-2 px-6 text-center">
+                                <span className="text-lg font-semibold text-gray-800 dark:text-white" id={data[item].id}>{data[item].quantity * data[item].price}$</span>
                             </td>
-                            <td class="py-2 px-6 text-center">
-                                <button class="text-red-500 hover:text-red-600 transition duration-300"
+                            <td className="py-2 px-6 text-center">
+                                <button className="text-red-500 hover:text-red-600 transition duration-300"
                                     id={data[item].id}
                                     onClick={hanldeDelete}><MdDelete className='text-3xl' /></button>
                             </td>
@@ -72,16 +124,15 @@ const Cart = () => {
         }
     }
     return (
-        <div class="container mx-auto dark:bg-slate-800">
-            <h1 class="text-4xl font-bold mb-8 text-center text-gray-800 dark:text-white">Shopping Cart</h1>
-
-            <table class="min-w-full bg-white border border-gray-300 rounded-lg shadow-lg dark:bg-slate-800">
-                <thead class="bg-gray-200 dark:bg-slate-800">
+        <div className="container mx-auto dark:bg-slate-800">
+            <h1 className="text-4xl font-bold mb-8 text-center text-gray-800 dark:text-white">Shopping Cart</h1>
+            <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow-lg dark:bg-slate-800">
+                <thead className="bg-gray-200 dark:bg-slate-800">
                     <tr>
-                        <th class="py-4 px-6 text-left text-lg font-semibold text-gray-700 dark:text-white">Product</th>
-                        <th class="py-4 px-6 text-lg font-semibold text-gray-700 dark:text-white">Quantity</th>
-                        <th class="py-4 px-6 text-lg font-semibold text-gray-700 dark:text-white">Price</th>
-                        <th class="py-4 px-6 text-lg font-semibold text-gray-700 dark:text-white">Action</th>
+                        <th className="py-4 px-6 text-left text-lg font-semibold text-gray-700 dark:text-white">Product</th>
+                        <th className="py-4 px-6 text-lg font-semibold text-gray-700 dark:text-white">Quantity</th>
+                        <th className="py-4 px-6 text-lg font-semibold text-gray-700 dark:text-white">Price</th>
+                        <th className="py-4 px-6 text-lg font-semibold text-gray-700 dark:text-white">Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -91,26 +142,26 @@ const Cart = () => {
             </table>
 
             {/* <!-- Tổng thanh toán --> */}
-            <div class="mt-8 bg-white shadow-lg rounded-lg p-6 dark:bg-slate-800">
-                <h2 class="text-2xl font-semibold mb-4 text-gray-800">Order Summary</h2>
+            <div className="mt-8 bg-white shadow-lg rounded-lg p-6 dark:bg-slate-800">
+                <h2 className="text-2xl font-semibold mb-4 text-gray-800">Order Summary</h2>
 
-                <div class="flex justify-between border-b pb-2 dark:text-white">
-                    <p class="text-lg">Subtotal:</p>
-                    <p class="text-lg font-semibold text-gray-800 dark:text-white">$49.98</p>
+                <div className="flex justify-between border-b pb-2 dark:text-white">
+                    <p className="text-lg">Subtotal:</p>
+                    <p className="text-lg font-semibold text-gray-800 dark:text-white">{total}$</p>
                 </div>
 
-                <div class="flex justify-between border-b py-2 dark:text-white">
-                    <p class="text-lg">Shipping:</p>
-                    <p class="text-lg font-semibold text-gray-800 dark:text-white">$5.00</p>
+                <div className="flex justify-between border-b py-2 dark:text-white">
+                    <p className="text-lg">Shipping:</p>
+                    <p className="text-lg font-semibold text-gray-800 dark:text-white">$5</p>
                 </div>
 
-                <div class="flex justify-between border-b py-2">
-                    <p class="text-lg font-semibold dark:text-white">Total:</p>
-                    <p class="text-2xl font-bold text-blue-600 dark:text-white">$54.98</p>
+                <div className="flex justify-between border-b py-2">
+                    <p className="text-lg font-semibold dark:text-white">Total:</p>
+                    <p className="text-2xl font-bold text-blue-600 dark:text-white">{total + 5}$</p>
                 </div>
 
-                <div class="flex justify-end mt-4">
-                    <button class="w-[200px] py-3 bg-primary text-white rounded-lg hover:scale-105 transform transition duration-300">Buy</button>
+                <div className="flex justify-end mt-4">
+                    <button className="w-[200px] py-3 bg-primary text-white rounded-lg hover:scale-105 transform transition duration-300">Buy</button>
                 </div>
             </div>
         </div>
